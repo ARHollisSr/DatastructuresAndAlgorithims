c7fa7dce5f7dd5a4b605683f13ce4bb836ddd4e7
FocusedBit <anthonyrayhollis@gmail.com>
2018-10-21 09:17:36 -0400

    Update firstPosEleminsertion.js

---
+++ firstPosEleminsertion.js
@@ -0,0 +1,12 @@
+Array.prototype.injectFirstPosition = function(element)
+{
+    for (let i = this.length; i >= 0; i--)
+    {
+        this[i] = this[i-1];
+    }
+    this[0] = element;
+}
+
+let someValues = [2,4,6,8,10,12];
+someValues.injectFirstPosition(0);
+console.log(someValues);
