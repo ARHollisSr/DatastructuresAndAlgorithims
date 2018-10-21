c7d54d7309d33cf2036514a1e1c31a3ee08a049b
FocusedBit <anthonyrayhollis@gmail.com>
2018-10-21 09:58:11 -0400

    added removeElemFirPos

--- Arrays/firstPosEleminsertion.js
+++ Arrays/firstPosEleminsertion.js
@@ -8,9 +8,9 @@ Array.prototype.injectFirstPosition = function(element)
 }
 
 let someValues = [2,4,6,8,10,12];
-//someValues.injectFirstPosition(0);
-//console.log(someValues);
-
-//alternatively 
+someValues.injectFirstPosition(0);
+console.log(someValues);
+//------------------------------------------------
+//In production use: 
 someValues.unshift(0);
 console.log(someValues);
---
+++ Arrays/removeElementFirstPosition.js
@@ -0,0 +1,22 @@
+
+Array.prototype.reIndex = function(myArray) {
+  const newArray = [];
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] !== undefined) {
+      newArray.push(myArray[i]);
+    }
+  }
+  return newArray;
+}
+
+    // remove first element manually and reIndex
+Array.prototype.removeTheFirstPosition = function() {
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i + 1];
+  }
+  return this.reIndex(this);
+};
+
+let someValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+someValues.removeTheFirstPosition();
+console.log(someValues);
