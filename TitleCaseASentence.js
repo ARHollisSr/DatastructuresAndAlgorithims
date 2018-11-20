ea15912973600eaad220ec7cc575942d5d480cf3
FocusedBit <anthonyrayhollis@gmail.com>
2018-11-20 05:55:21 -0500

    added gitignore

---
+++ TitleCaseASentence.js
@@ -0,0 +1,9 @@
+function titleCase(str) {
+    var stringToArray = str.toLowerCase().split(' ');
+    var result = stringToArray.map((val)=> {
+        return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+    });
+    return result.join(" ");
+}
+
+console.log(titleCase("I'm a little tea pot"));
