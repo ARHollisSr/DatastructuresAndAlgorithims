a23f2db2f9956b34e630657da145ca759ab85ca3
FocusedBit <anthonyrayhollis@gmail.com>
2018-11-22 23:08:14 -0500

    Update Mutation.js

--- Mutation.js
+++ Mutation.js
@@ -12,6 +12,7 @@ function mutation(arr) {
 
 function mutatio2(arr) {
   return arr[1].toLowerCase().split('').every(function(letter) {
+      
     return arr[0].toLowerCase().indexOf(letter) != -1;
   });
 }
